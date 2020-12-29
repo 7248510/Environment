951735c2cc86e1c07b1f40c98d623d9f612b242d
7248510 <calebkre19@gmail.com>
2020-12-29 18:24:15 -0500

    Identify works. I tested the code on Ubuntu & Windows. The only thing that needs to be figured out is headerfiles!

--- main.cc
+++ main.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "identify.c"
-//#include "win32.cc"
+#include "win32.cc" //Fix Linux dependency errors(I need to figure out how to seperate windows header files and Linux headers)
 //cl main.cc /EHsc && main.exe
 //cl test.cc /EHsc && test.exe
 int main()
